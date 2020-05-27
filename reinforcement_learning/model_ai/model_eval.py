ab0ac4c41aef573940eb7617aca124c8bbceda5c
Ray Wong <ray.wkwong@gmail.com>
2020-05-27 17:44:32 +0800

    add log

--- reinforcement_learning/model_ai/model_eval.py
+++ reinforcement_learning/model_ai/model_eval.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import game_env.game_env_robot as ger
 import game_env.game_env as ge
 import reinforcement_learning.model_ai.model as rm 
+import logging as l
 
 def color_index_to_name(color_index):
     import game_env.game_env as ge
@@ -20,15 +21,21 @@ def play_game(env, ai):
     game_end = False
     while not game_end:
         logits = ai( np.atleast_2d(state) )
-        probs = tf.nn.softmax(logits)
 
-        action = np.random.choice(env.action_size, p=probs.numpy()[0])
+        action = np.argmax(logits[0])
+
+        # probs = tf.nn.softmax(logits)
+        # action = np.random.choice(env.action_size, p=probs.numpy()[0])
 
         state, game_end, valid_move, player_won, robot_won = env.step( action  )
 
     return player_won, robot_won, valid_move
 
 def eval_model_ai(n=100):
+
+    l.info('')
+    l.info(' evalulate model ai. games to play : {}'.format(n))
+    l.info('')
     env = ger.Env()
     ai = rm.MyModel()
 
@@ -41,4 +48,5 @@ def eval_model_ai(n=100):
         stats[i,1] = robot_won
         stats[i,2] = valid_move 
 
+    l.info(' evalulate model ai ends')
     return stats
